2d99bf8445fcc32e8574f79cc0a6b304642aeb4e
Konstantin Zabelin <zabelin.ks@gmail.com>
2018-08-06 23:52:00 -0700

    Added couple todo's

---
+++ Tests/SwiftygramTests/APITests.swift
@@ -0,0 +1,21 @@
+//
+// Created by Zap on 06.08.2018.
+//
+
+import Foundation
+import XCTest
+
+@testable import Swiftygram
+
+
+final class APITests: XCTestCase {
+
+    func test_API_parses_error() {
+        XCTFail("TODO: Implement")
+    }
+
+    func test_API_parses_success() {
+        XCTFail("TODO: Implement")
+    }
+}
+
---
+++ Tests/SwiftygramTests/BotTests.swift
@@ -0,0 +1,24 @@
+//
+// Created by Zap on 06.08.2018.
+//
+
+import Foundation
+import XCTest
+
+@testable import Swiftygram
+
+
+final class BotTests: XCTestCase {
+
+    func test_Bot_receives_info_about_itself() {
+        XCTFail("TODO: Implement")
+    }
+
+    func test_Bot_sends_message_to_itself() {
+        XCTFail("TODO: Implement")
+    }
+
+    func test_Bot_receives_message() {
+        XCTFail("TODO: Implement")
+    }
+}
