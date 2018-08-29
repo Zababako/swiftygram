cd43963e294ddd34875cebb9e53f6da109f88d92
Konstantin Zabelin <zabelin.ks@gmail.com>
2018-08-29 08:46:24 -0700

    Made APIMock a bit more real

--- Tests/SwiftygramTests/APIMock.swift
+++ Tests/SwiftygramTests/APIMock.swift
@@ -17,28 +17,34 @@ final class APIMock: API {
     var t_storage: [URL : Result<Any>] = [:]
 	var t_onSend: (URLRequest) -> Void = { _ in }
 
+    var t_queue: DispatchQueue = DispatchQueue(label: "com.zababako.swiftygram.tests.apimock")
+
     func send<T: Decodable>(request: URLRequest, onComplete: @escaping (Result<T>) -> Void) {
 
 		t_onSend(request)
-		
+
+        let finalizer: (Result<T>) -> Void = {
+            [t_queue] result in t_queue.async { onComplete(result) }
+        }
+
         guard let preparedResult = t_storage[request.url!] else {
             XCTFail("Test preparation - result for: \(request.url!) is prepared")
-            onComplete(.failure(TestError.failedPreparation))
+            finalizer(.failure(TestError.failedPreparation))
             return
         }
 
         guard case .success(let object) = preparedResult else {
-            onComplete(preparedResult.map { $0 as! T})
+            finalizer(preparedResult.map { $0 as! T})
             return
         }
 
         guard let castedObject = object as? T else {
             XCTFail("Test preparation - result for: \(request.url!) has wrong Type")
-            onComplete(.failure(TestError.failedPreparation))
+            finalizer(.failure(TestError.failedPreparation))
             return
         }
 
-        onComplete(.success(castedObject))
+        finalizer(.success(castedObject))
     }
 }
 
