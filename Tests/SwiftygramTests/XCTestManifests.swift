c5183fda8642ccfc1fede880f69902fb9e77ffb9
Konstantin Zabelin <zabelin.ks@gmail.com>
2018-08-30 01:51:36 -0700

    Fixed issue with testing on a wrong platform

--- Tests/SwiftygramTests/XCTestManifests.swift
+++ Tests/SwiftygramTests/XCTestManifests.swift
@@ -58,24 +58,6 @@ extension ToolsTests {
     ]
 }
 
-#if os(macOS) // To make it compilable with 'fastlane scan' - for code coverage generation
-public typealias XCTestCaseClosure = (XCTestCase) throws -> Void
-public typealias XCTestCaseEntry = (testCaseClass: XCTestCase.Type, allTests: [(String, XCTestCaseClosure)])
-public func testCase<T: XCTestCase>(_ allTests: [(String, (T) -> () -> Void)]) -> XCTestCaseEntry {
-    let tests: [(String, XCTestCaseClosure)] = allTests.map { ($0.0, test($0.1)) }
-    return (T.self, tests)
-}
-private func test<T: XCTestCase>(_ testFunc: @escaping (T) -> () throws -> Void) -> XCTestCaseClosure {
-    return { testCaseType in
-        guard let testCase = testCaseType as? T else {
-            fatalError("Attempt to invoke test on class \(T.self) with incompatible instance type \(type(of: testCaseType))")
-        }
-
-        try testFunc(testCase)()
-    }
-}
-#endif
-
 #if !os(macOS)
 public func __allTests() -> [XCTestCaseEntry] {
     return [
